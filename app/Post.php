916b6b9bf110f57a9586905cb69eaccbe019cbdf
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-11-16 15:48:19 -0500

    updating debug channels

--- app/Post.php
+++ app/Post.php
@@ -146,8 +146,7 @@ class Post extends Model {
 
   public static function getEventData($eventId, $profile) {
     if (! Post::where('fb_post_id', '=', $eventId)->first()) {
-      dd($eventId);
-      $client = new Client(['base_uri' => 'https://graph.facebook.com/v2.8']);
+      $client = new Client(['base_uri' => 'https://graph.facebook.com/v2.8/']);
       try {
         $response = $client->request('GET', $eventId, [
           'query' => ['access_token' => $profile->fb_app_id]
