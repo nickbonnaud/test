071214a1c39a3f1ed8d8ee8772efc63b6e1f43c5
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-11-16 11:42:59 -0500

    modifying csrf middleware

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-    	'api/web/connections/*'
+    	'api/web/connections/*',
+    	'api/web/posts/subscriptions'
     ];
 }
