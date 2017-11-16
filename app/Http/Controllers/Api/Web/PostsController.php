4e729b098c0a9e60978fdeac7c56feffb829c258
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-11-16 11:47:53 -0500

    modifying csrf middleware

--- app/Http/Controllers/Api/Web/PostsController.php
+++ app/Http/Controllers/Api/Web/PostsController.php
@@ -24,6 +24,7 @@ class PostsController extends Controller
   }
 
   public function store(Request $request) {
+    return response()->json("hello");
     event(new Debug('init'));
     $body = $request->getContent();
     event(new Debug($body));
