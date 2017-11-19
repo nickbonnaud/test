c1f6f14f9a2f3f1e08883c2344aca88bf4cd3445
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-11-18 20:30:48 -0500

    fixing webhook

--- app/Http/Controllers/Api/Web/AccountsController.php
+++ app/Http/Controllers/Api/Web/AccountsController.php
@@ -12,7 +12,8 @@ class AccountsController extends Controller {
 	public function update(Request $request) {
 		Log::info("new work");
 		$business = json_decode($request->getContent(), true);
-		Log::info($business);
+
+		Log::info(array_get($business, 'merchantData.merchantId'));
    
 	}
 }
