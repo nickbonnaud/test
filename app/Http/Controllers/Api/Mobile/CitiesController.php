6b108fcd0b4adbc83cd27baeb081ee72c2270cc6
Nick Bonnaud <nick.bonnaud@gmail.com>
2017-12-05 10:19:52 -0500

    testing mobile api

--- app/Http/Controllers/Api/Mobile/CitiesController.php
+++ app/Http/Controllers/Api/Mobile/CitiesController.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Controller;
 class CitiesController extends Controller {
 
 	public function index(Request $request) {
-		return response()->json($request->input('lat'));
+		return response()->json('Hello');
 		$city = City::getCurrentLocation($request->input('lat'), $request->input('lng'));
     return response()->json(['city' => $city]);
 	}
