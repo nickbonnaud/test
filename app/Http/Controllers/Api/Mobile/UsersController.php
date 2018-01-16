9dd528ab6cde370cd2dab75aef124dd25da75a5b
Nick Bonnaud <nick.bonnaud@gmail.com>
2018-01-16 08:01:36 -0500

    testing photo uploads

--- app/Http/Controllers/Api/Mobile/UsersController.php
+++ app/Http/Controllers/Api/Mobile/UsersController.php
@@ -26,7 +26,7 @@ class UsersController extends Controller {
 	}
 
 	public function update(Request $request) {
-		$data = $request->getContent('something');
+		$data = $request->all();
 		
 		return response($data);
 	}
