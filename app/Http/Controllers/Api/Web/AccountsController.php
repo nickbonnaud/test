ef0d0e9468e59bc5db503c19ef53c8ac170de5e3
Nick Bonnaud <nick.bonnaud@gmail.com>
2018-01-30 11:04:09 -0500

    updating geofences

--- app/Http/Controllers/Api/Web/AccountsController.php
+++ app/Http/Controllers/Api/Web/AccountsController.php
@@ -10,6 +10,14 @@ use App\Http\Controllers\Controller;
 class AccountsController extends Controller {
 
 	public function update(Request $request) {
-		return response('hit it');
+		Log::info("INIT!!!");
+		$business = json_decode($request->getContent(), true);
+		Log::info($business);
+		$merchantId = array_get($business, 'merchantData.merchantId');
+		$status = array_get($business, 'merchantData.status');
+
+    $account = Account::where('splash_id', '=', $merchantId)->first();
+    $account->status = $status;
+    $account->save();
 	}
 }
