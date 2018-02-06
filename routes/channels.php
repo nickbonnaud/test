37024d75b5463de6131d358fd841cdf2c9bcf976
Nick Bonnaud <nick.bonnaud@gmail.com>
2018-02-06 17:57:37 -0500

    update

--- routes/channels.php
+++ routes/channels.php
@@ -46,6 +46,6 @@ Broadcast::channel('redeemed-item.{profile}', function ($user, Profile $profile)
   return $user->profile->slug === $profile->slug;
 });
 
-Broadcast::channel('customer.{id}', function ($user) {
+Broadcast::channel('customer.{id}', function ($user, $id) {
 	return true;
 });
