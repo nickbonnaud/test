943f47aa90c298dee64bf2de45c92e7b8a0e3d17
Nick Bonnaud <nick.bonnaud@gmail.com>
2018-02-06 18:00:09 -0500

    update

--- app/Events/CustomerBillUpdate.php
+++ app/Events/CustomerBillUpdate.php
@@ -23,6 +23,6 @@ class CustomerBillUpdate implements ShouldBroadcast
 
   public function broadcastOn()
   {
-    return new PrivateChannel('customer.124');
+    return new PrivateChannel('customer.' . $this->user->id);
   }
 }
