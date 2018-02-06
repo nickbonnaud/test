09040b2b93f43a8d9b94638c968e7856f8f09fc3
Nick Bonnaud <nick.bonnaud@gmail.com>
2018-02-06 17:42:33 -0500

    testing pusher

--- public/js/app.js
+++ public/js/app.js
@@ -89155,7 +89155,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
   mounted: function mounted() {
     var _this = this;
 
-    console.log('hereeref');
     Echo.private('geofence.' + this.profile.slug).listen('CustomerBreakGeoFence', function (event) {
       _this.checkType(event);
     });
@@ -89163,8 +89162,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
       _this.setItemRedeemed(event);
     });
 
-    console.log('tada!');
-    Echo.private('customer.124').listen('CustomerBillUpdate', function (event) {
+    Echo.private('customer').listen('CustomerBillUpdate', function (event) {
       console.log('triggered');
       console.log(event);
     });
