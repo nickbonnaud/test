79cdf345698a5e4d508149651d0c495cb328005e
Nick Bonnaud <nick.bonnaud@gmail.com>
2018-02-07 08:18:47 -0500

    update

--- app/Events/CustomerBillUpdate.php
+++ app/Events/CustomerBillUpdate.php
@@ -16,13 +16,27 @@ class CustomerBillUpdate implements ShouldBroadcast
 
   public $transaction;
 
-  public function __construct($transaction)
-  {
+  public function __construct($transaction) {
     $this->transaction = $transaction;
   }
 
-  public function broadcastOn()
-  {
+  public function broadcastWith() {
+    return [
+      'id' => $this->transaction->id,
+      'business_name' => $this->transaction->profile->business_name,
+      'logo' => $this->transaction->profile->logo->apiUrl,
+      'products' => $this->transaction->products,
+      'tax' => $this->transaction->tax,
+      'tips' => $this->transaction->tips,
+      'net_sales' => $this->transaction->net_sales,
+      'total' => $this->transaction->total,
+      'purchased_on' => $this->transaction->created_at,
+      'status' => $this->transaction->status,
+      'bill_closed' => $this->transaction->bill_closed
+    ];
+  }
+
+  public function broadcastOn() {
     return new PrivateChannel('customer.' . $this->transaction->user_id);
   }
 }
