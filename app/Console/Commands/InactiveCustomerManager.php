b06836c9cf59d4ab5c67ed9f51d3b93832d26fc3
Nick Bonnaud <nick.bonnaud@gmail.com>
2018-04-10 16:07:31 -0400

    update

--- app/Console/Commands/InactiveCustomerManager.php
+++ app/Console/Commands/InactiveCustomerManager.php
@@ -54,6 +54,7 @@ class InactiveCustomerManager extends Command
   }
 
   public static function sendNotificationOrPay($lastNotification, $transaction, $userLocation) {
+    dd(str_replace_first("App\\Notifications\\",'', $lastNotification->type));
     switch (str_replace_first("App\\Notifications\\",'', $lastNotification->type)) {
       case 'FixTransactionNotification':
         self::fixTransactionOrPay($transaction, $userLocation);
