bc347be92adb7cffa3148021b6adedab1bf5a525
Nick Bonnaud <nick.bonnaud@gmail.com>
2018-05-30 10:55:36 -0400

    update

--- app/Http/Resources/PayProfileResource.php
+++ app/Http/Resources/PayProfileResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\Resource;
 
 class PayProfileResource extends Resource
@@ -14,7 +15,10 @@ class PayProfileResource extends Resource
       'slug' => $this->slug,
       'business_name' => $this->business_name,
       'logo' =>  $this->logo->apiUrl,
-      'token' => $this->token
+      'token' => [
+      	'value' => $this->token,
+      	'expiry' => $this->token ? Carbon::now()->addMinutes(env('JWT_TTL'))->timestamp : null
+      ]
     ];
   }
 }
