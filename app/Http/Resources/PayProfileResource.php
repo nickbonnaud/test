6cb576fa60a1b662e11ab4dc5b68fd421d47acf4
Nick Bonnaud <nick.bonnaud@gmail.com>
2018-06-28 15:35:46 -0400

    update

--- app/Http/Resources/PayProfileResource.php
+++ app/Http/Resources/PayProfileResource.php
@@ -10,12 +10,13 @@ class PayProfileResource extends Resource
 
   public function toArray($request)
   {
+    $connectedPos = $this->connectedPos()->first();
     return [
       'id' => $this->id,
       'slug' => $this->slug,
       'business_name' => $this->business_name,
       'logo' =>  $this->logo->apiUrl,
-      'connected_pos' => $this->connectedPos() ? ($this->connectedPos()->first())->account_type : null,
+      'connected_pos' => $connectedPos ? $connectedPos->account_type : null,
       'token' => [
       	'value' => $this->token ? $this->token : null,
       	'expiry' => $this->token ? Carbon::now()->addMinutes(env('JWT_TTL'))->timestamp : null
