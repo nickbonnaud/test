6727e1b20d2f0ba56c39eff1d565f20d42c7ce90
Nick Bonnaud <nick.bonnaud@gmail.com>
2018-06-28 22:28:29 -0400

    update

--- app/ConnectedPos.php
+++ app/ConnectedPos.php
@@ -33,4 +33,31 @@ class ConnectedPos extends Model
     }
     dd("success " . $response->getBody());
   }
+
+  public function createPockeytCustomer() {
+    $client = new Client(['base_uri' => env('CLOVER_BASE_URL')]);
+    try {
+      $response = $client->request('POST', 'v3/merchants/' . $this->merchant_id . '/items', [
+        'headers' => [
+          'Authorization' => 'Bearer ' . $this->token,
+          'Accept' => 'application/json'
+        ],
+        'json' => [
+          'name' => 'Test Customer',
+          'priceType' => 'FIXED',
+          'isRevenue' => false,
+          'price' => 0,
+          'id' => 'testcustomer1',
+          'categories' => [
+            'Pockeyt Customers'
+          ]
+        ]
+      ]);
+    } catch (GuzzleException $e) {
+      if ($e->hasResponse()) {
+        dd("error: " . $e->getResponse());
+      }
+    }
+    dd("success " . $response->getBody());
+  }
 }
