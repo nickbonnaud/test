778cc21e8f95d73a97606fc49ca94392803c549b
Nick Bonnaud <nick.bonnaud@gmail.com>
2018-06-29 10:48:42 -0400

    update

--- app/Http/Controllers/Api/Mobile/PayWebHooksController.php
+++ app/Http/Controllers/Api/Mobile/PayWebHooksController.php
@@ -8,7 +8,10 @@ use App\Http\Controllers\Controller;
 class PayWebHooksController extends Controller {
 
 	public function clover(Request $request) {
-		\Log::debug($request->all());
-		return response()->json(['success' => 'CONNECTED'], 200);
+		if ($request->header('X-Clover-Auth') == env('CLOVER_WEBHOOK_HEADER')) {
+			return response()->json(['success' => 'authorized'], 200);
+		} else {
+			return response()->json(['error' => 'Unauthorized'], 401);
+		}
 	}
 }
