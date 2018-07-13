8d5cab589bd127a41d082c0e97ea11c4e3aefd53
Nick Bonnaud <nick.bonnaud@gmail.com>
2018-07-13 16:23:37 -0400

    update

--- app/ConnectedPos.php
+++ app/ConnectedPos.php
@@ -267,13 +267,10 @@ class ConnectedPos extends Model
   public function deleteClosedCloverTransaction($orderId) {
     $client = new Client(['base_uri' => env('CLOVER_BASE_URL')]);
     try {
-      $response = $client->request('POST', 'v3/merchants/' . $this->merchant_id . '/orders/' . $orderId, [
+      $response = $client->request('DELETE', 'v3/merchants/' . $this->merchant_id . '/orders/' . $orderId, [
         'headers' => [
           'Authorization' => 'Bearer ' . $this->token,
           'Accept' => 'application/json'
-        ],
-        'json' => [
-          'state' => null 
         ]
       ]);
     } catch (ClientErrorResponseException $exception) {
