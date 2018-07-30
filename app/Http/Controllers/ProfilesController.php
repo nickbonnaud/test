95342f596ab6c74f565d4ca31bad3826d792eeaf
Nick Bonnaud <nick.bonnaud@gmail.com>
2018-07-30 14:20:26 -0400

    update

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -98,13 +98,13 @@ class ProfilesController extends Controller
     }
 
     public function test() {
-        // UserLocation::create([
-        //     'profile_id' => 1,
-        //     'user_id' => 288
-        // ]);
+        UserLocation::create([
+            'profile_id' => 1,
+            'user_id' => 288
+        ]);
 
-        $userLocation = UserLocation::where('id', 24)->first();
-        $userLocation->removeLocation();
+        // $userLocation = UserLocation::where('id', 24)->first();
+        // $userLocation->removeLocation();
         // $transaction = Transaction::where('id', 482)->first();
         // $connectedPos = $transaction->profile->connectedPos;
         // $connectedPos->test();
