f624fa50c877d4bb2ccd9d85efcf8c2c44464ebf
Nick Bonnaud <nick.bonnaud@gmail.com>
2018-08-21 09:29:06 -0400

    update

--- tests/Feature/CloverTipsTest.php
+++ tests/Feature/CloverTipsTest.php
@@ -125,4 +125,50 @@ class CloverTipsTest extends TestCase
     $this->assertEquals($transactionId1, $response->data[1]->transaction_id);
     $this->assertCount(2, $response->data);
   }
+
+  function test_an_authorized_user_can_get_tips_for_specific_employees_for_specific_times() {
+    $user = create('App\User');
+    $profile = create('App\Profile', ['user_id' => $user->id]);
+
+    $employeeId = '1234qwer';
+    $transactionId = "vmdih37";
+
+    $transactionOfEmployee = create('App\Transaction', ['profile_id' => $profile->id, 'employee_id' => $employeeId, 'status' => 20, 'refund_full' => false, 'pos_transaction_id' => $transactionId, 'created_at' => (Carbon::now())->subDay()]);
+
+    $transactionId0 = "fheuhf3";
+
+    $transactionOfEmployee = create('App\Transaction', ['profile_id' => $profile->id, 'employee_id' => $employeeId, 'status' => 20, 'refund_full' => false, 'pos_transaction_id' => $transactionId0, 'created_at' => (Carbon::now())->subDays(5)]);
+
+    $employeeId1 = 'vfjkd7s';
+    $transactionId1 = "bjcsbjvd";
+
+    $transactionOfEmployee1 = create('App\Transaction', ['profile_id' => $profile->id, 'employee_id' => $employeeId1, 'status' => 20, 'refund_full' => false, 'pos_transaction_id' => $transactionId1, 'created_at' => (Carbon::now())->subDay()]);
+
+    $transaction01 = "cbdu478";
+    $transactionOfEmployee1 = create('App\Transaction', ['profile_id' => $profile->id, 'employee_id' => $employeeId1, 'status' => 20, 'refund_full' => false, 'pos_transaction_id' => $transaction01, 'created_at' => (Carbon::now())->subDays(6)]);
+
+    $employeeId2 = 'frnj444';
+    $transactionId2 = 'cbdhsvhdsv';
+
+    $transactionOfEmployee2 = create('App\Transaction', ['profile_id' => $profile->id, 'employee_id' => $employeeId2, 'status' => 20, 'refund_full' => false, 'pos_transaction_id' => $transactionId2, 'created_at' => (Carbon::now())->subDay()]);
+
+
+    $startDate = ((Carbon::now())->subDay())->subHours(2)->toDateString();
+    $startTime = ((Carbon::now())->subDay())->subHours(2)->toTimeString();
+    $startDateTime = $startDate . '_' . $startTime;
+
+    $endDate= ((Carbon::now())->subDay())->addHours(6)->toDateString();
+    $endTime = ((Carbon::now())->subDay())->addHours(6)->toTimeString();
+    $endDateTime = $endDate . '_' . $endTime;
+
+
+    $response = $this->get("/api/mobile/pay/tips?startTime={$startDateTime}&endTime={$endDateTime}&employeeTips[]={$employeeId}&employeeTips[]={$employeeId1}", $this->headers($user))->getData();
+
+    $this->assertEquals($employeeId1, $response->data[1]->employee_id);
+    $this->assertEquals($transactionId1, $response->data[1]->transaction_id);
+
+    $this->assertEquals($employeeId, $response->data[0]->employee_id);
+    $this->assertEquals($transactionId, $response->data[0]->transaction_id);
+    $this->assertCount(2, $response->data);
+  }
 }
