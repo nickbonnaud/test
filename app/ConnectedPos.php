12432339cf15c41d81edc217f2bb761be4fff072
Nick Bonnaud <nick.bonnaud@gmail.com>
2018-08-24 16:17:05 -0400

    update

--- app/ConnectedPos.php
+++ app/ConnectedPos.php
@@ -46,26 +46,6 @@ class ConnectedPos extends Model {
     $client = new Client(['base_uri' => env('CLOVER_BASE_URL')]);
 
     try {
-      $response = $client->request("GET", "v3/merchants", [
-        'headers' => [
-          'Authorization' => 'Bearer ' . $this->token,
-          'Accept' => 'application/json'
-        ]
-      ]);
-    } catch (ClientErrorResponseException $exception) {
-      dd($exception->getResponse()->getBody(true));
-    }
-    $body = json_decode($response->getBody());
-    dd($body);
-
-
-
-
-
-
-    $client = new Client(['base_uri' => env('CLOVER_BASE_URL')]);
-
-    try {
       $response = $client->request("GET", "v3/merchants/{$this->merchant_id}/categories", [
         'headers' => [
           'Authorization' => 'Bearer ' . $this->token,
@@ -190,7 +170,6 @@ class ConnectedPos extends Model {
   }
 
   public function parseWebHookData($webHookData) {
-    \Log::debug($webHookData);
     foreach ($webHookData as $webData) {
       if (starts_with($webData['objectId'], 'A')) {
         $this->deleteConnectedPos($webData);
@@ -202,7 +181,7 @@ class ConnectedPos extends Model {
 
   public function deleteConnectedPos($webData) {
     if ($webData['type'] == 'DELETE') {
-      
+      $this->delete();
     }
   }
 
