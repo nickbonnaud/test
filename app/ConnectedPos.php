284accd3d3801084875361b71f66f757fc5633b9
Nick Bonnaud <nick.bonnaud@gmail.com>
2018-08-24 13:29:45 -0400

    update

--- app/ConnectedPos.php
+++ app/ConnectedPos.php
@@ -77,10 +77,10 @@ class ConnectedPos extends Model {
     
   }
 
-  public function checkIfCloverCustomerExists() {
+  public function checkIfCloverCustomerExists($userLocation) {
     $client = new Client(['base_uri' => env('CLOVER_BASE_URL')]);
     try {
-      $response = $client->request("GET", "v3/merchants/{$this->merchant_id}/items", [
+      $response = $client->request("GET", "v3/merchants/{$this->merchant_id}/categories/{$this->clover_category_id}", [
         'headers' => [
           'Authorization' => 'Bearer ' . $this->token,
           'Accept' => 'application/json'
--- config/constants.php
+++ config/constants.php
@@ -2,5 +2,6 @@
 return [
 	'clover' => [
 		'category' => 'Pockeyt Customers',
+		'customer_prefix' => 'pockeyt-'
 	]
 ];
