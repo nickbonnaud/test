2f489a08c281f2fd46a6a710707b38bc34c8de67
Nick Bonnaud <nick.bonnaud@gmail.com>
2018-08-24 12:56:45 -0400

    update

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -100,7 +100,7 @@ class ProfilesController extends Controller
 
     public function enter() {
         $profile = Profile::where('id', 1)->first();
-        $profile->connectedPos->checkIfCategoryExistsInClover();
+        $profile->connectedPos->addPockeytCustomersCategory();
 
 
 
